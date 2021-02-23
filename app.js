b796c44273b07cc505b627dbaf822e42e08eebe3
developer-junaid <qjunaid623@gmail.com>
2021-02-23 09:33:54 +0500

    Added Basic HTML, Basic CSS, Basic JS
    NEXT: Style Todo Items

---
+++ app.js
@@ -0,0 +1,43 @@
+// Selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+// Event Listeners
+todoButton.addEventListener("click", addTodo);
+
+// Functions
+function addTodo(event) {
+  event.preventDefault(); // Prevent form from submitting
+
+  // Todo Div
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+
+  // Create li
+  const newTodo = document.createElement("li");
+  newTodo.innerText = "Hey";
+  newTodo.classList.add("todo-item");
+
+  // Add new Todo to todoDiv
+  todoDiv.appendChild(newTodo);
+
+  // Checkmark button
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add("complete-btn");
+
+  // Add Button to todoDiv
+  todoDiv.appendChild(completedButton);
+
+  // Trash Button
+  const trashButton = document.createElement("button");
+  completedButton.innerHTML = '<i class="fas fa-trash"></i>';
+  completedButton.classList.add("complete-btn");
+
+  // Add Button to todoDiv
+  todoDiv.appendChild(trashButton);
+
+  // Attach the Big Div to actual ul - Todo List
+  todoList.appendChild(todoDiv);
+}
